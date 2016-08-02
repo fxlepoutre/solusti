73d1a046195f2675b59b428e7b8b26b4c9e9f343
fxlepoutre <fxlepoutre@gmail.com>
2016-08-02 03:18:35 +0000

    On branch master
     Initial commit
    
     Changes to be committed:
    	new file:   css/animate.min.css
    	new file:   css/bootstrap.min.css
    	new file:   css/font-awesome.min.css
    	new file:   css/lightbox.css
    	new file:   css/main.css
    	new file:   css/responsive.css
    	new file:   css/solusti-colors.css
    	new file:   fonts/FontAwesome.otf
    	new file:   fonts/fontawesome-webfont.eot
    	new file:   fonts/fontawesome-webfont.svg
    	new file:   fonts/fontawesome-webfont.ttf
    	new file:   fonts/fontawesome-webfont.woff
    	new file:   fonts/fontawesome-webfont.woff2
    	new file:   fonts/glyphicons-halflings-regular.eot
    	new file:   fonts/glyphicons-halflings-regular.svg
    	new file:   fonts/glyphicons-halflings-regular.ttf
    	new file:   fonts/glyphicons-halflings-regular.woff
    	new file:   fonts/glyphicons-halflings-regular.woff2
    	new file:   images/contact-bg-old.jpg
    	new file:   images/contact-bg.jpg
    	new file:   images/favicon.ico
    	new file:   images/lightbox/close.png
    	new file:   images/lightbox/loading.gif
    	new file:   images/lightbox/next.png
    	new file:   images/lightbox/prev.png
    	new file:   images/logo2_sm.png
    	new file:   images/slider-bg.jpg
    	new file:   index.php
    	new file:   js/bootstrap.min.js
    	new file:   js/html5shiv.js
    	new file:   js/jquery.countTo.js
    	new file:   js/jquery.inview.min.js
    	new file:   js/jquery.js
    	new file:   js/lightbox.min.js
    	new file:   js/main.js
    	new file:   js/mousescroll.js
    	new file:   js/respond.min.js
    	new file:   js/smoothscroll.js
    	new file:   js/wow.min.js
    	new file:   languages/lang.en.php
    	new file:   sendemail.php

---
+++ index.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>Solusti | Your partner to successful WMS</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/animate.min.css" rel="stylesheet"> 
+  <link href="css/font-awesome.min.css" rel="stylesheet">
+  <link href="css/lightbox.css" rel="stylesheet">
+  <link href="css/main.css" rel="stylesheet">
+  <link href="css/solusti-colors.css" rel="stylesheet">
+  <link href="css/responsive.css" rel="stylesheet">
+
+  <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+  <![endif]-->
+  
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'>
+  <link rel="shortcut icon" href="images/favicon.ico">
+</head><!--/head-->
+
+<body>
+
+  <!--.preloader-->
+  <div class="preloader"> <i class="fa fa-circle-o-notch fa-spin"></i></div>
+  <!--/.preloader-->
+
+  <header id="home">
+    <div id="home-slider" class="carousel slide carousel-fade" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="item active" style="background-image: url(images/slider-bg.jpg)">
+          <div class="caption">
+            <h1 class="animated fadeInLeftBig">JDA / RedPrairie WMS solutions for your <span>warehouse</span></h1>
+            <p class="animated fadeInRightBig">Need training, customized reports, troubleshooting or integration?</p>
+            <a data-scroll class="btn btn-start animated fadeInUpBig" href="#contact-us">Contact us now</a>
+          </div>
+        </div>
+      </div>
+      <a id="tohash" href="#services"><i class="fa fa-angle-down"></i></a>
+    </div><!--/#home-slider-->
+
+    <div class="main-nav">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="">
+            <h1><img class="img-responsive" src="images/logo2_sm.png" alt="logo"></h1>
+          </a>                    
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav navbar-right">                 
+            <li class="scroll active"><a href="#home">Home</a></li>
+            <li class="scroll"><a href="#services">Services</a></li>
+            <li class="scroll"><a href="#contact">Contact</a></li>       
+          </ul>
+        </div>
+      </div>
+    </div><!--/#main-nav-->
+  </header><!--/#home-->
+  
+  <section id="services">
+    <div class="container">
+      <div class="heading wow fadeInUp" data-wow-duration="500ms" data-wow-delay="150ms">
+        <div class="row">
+          <div class="text-center col-sm-8 col-sm-offset-2">
+            <h2>Our Services</h2>
+            <p>Here's what we offer. Contact us for more details.</p>
+          </div>
+        </div> 
+      </div>
+      <div class="text-center our-services">
+        <div class="row">
+          <div class="col-sm-3 wow fadeInDown" data-wow-duration="500ms" data-wow-delay="150ms">
+            <div class="service-icon one">
+              <i class="fa fa-graduation-cap"></i>
+            </div>
+            <div class="service-info">
+              <h3>Trainings</h3>
+              <p>Go through a discovery session of JDA and/or train functional and technical teams on how to use and configure JDA.</p>
+              <p><a data-scroll class="btn" href="#contact-us">Schedule your training now!</a></p>
+            </div>
+          </div>
+          <div class="col-sm-3 wow fadeInDown" data-wow-duration="500ms" data-wow-delay="250ms">
+            <div class="service-icon two">
+              <i class="fa fa-line-chart"></i>
+            </div>
+            <div class="service-info">
+              <h3>Reports</h3>
+              <p>Develop and customize business reports: create documents for daily usage or extract data for business analysis.</p>
+              <p><a data-scroll class="btn" href="#contact-us">Get your reports now!</a></p>
+            </div>
+          </div>
+          <div class="col-sm-3 wow fadeInUp" data-wow-duration="500ms" data-wow-delay="350ms">
+            <div class="service-icon three">
+              <i class="fa fa-search"></i>
+            </div>
+            <div class="service-info">
+              <h3>Troubleshooting</h3>
+              <p>Diagnose technical and functional issues on your system and processes.</p>
+              <p><a data-scroll class="btn" href="#contact-us">Get assistance now!</a></p>
+            </div>
+          </div>
+          <div class="col-sm-3 wow fadeInUp" data-wow-duration="500ms" data-wow-delay="450ms">
+            <div class="service-icon four">
+              <i class="fa fa-connectdevelop"></i>
+            </div>
+            <div class="service-info">
+              <h3>Integration</h3>
+              <p>Build integration with external systems, connect your ERP with the WMS (orders, shipments, inventory).</p>
+              <p><a data-scroll class="btn" href="#contact-us">Start your integration now!</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section><!--/#services-->
+
+  <section id="contact">
+    <div id="google-map" class="wow fadeIn" data-latitude="48.8380243" data-longitude="2.3905071" data-wow-duration="500ms" data-wow-delay="400ms"></div>
+    <div id="contact-us" class="parallax">
+      <div class="container">
+        <div class="row">
+          <div class="heading text-center col-sm-8 col-sm-offset-2 wow fadeInUp" data-wow-duration="500ms" data-wow-delay="150ms">
+            <h2>Contact Us</h2>
+          </div>
+        </div>
+        <div class="contact-form wow fadeIn" data-wow-duration="500ms" data-wow-delay="300ms">
+          <div class="row">
+            <div class="col-sm-6">
+              <form id="main-contact-form" name="contact-form" method="post" action="sendemail.php">
+                <div class="row  wow fadeInUp" data-wow-duration="500ms" data-wow-delay="150ms">
+                  <div class="col-sm-6">
+                    <div class="form-group">
+                      <input type="text" name="name" class="form-control" placeholder="Name" required="required">
+                    </div>
+                  </div>
+                  <div class="col-sm-6">
+                    <div class="form-group">
+                      <input type="email" name="email" class="form-control" placeholder="Email Address" required="required">
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <input type="text" name="subject" class="form-control" placeholder="Subject" required="required">
+                </div>
+                <div class="form-group">
+                  <textarea name="message" id="message" class="form-control" rows="4" placeholder="Enter your message" required="required"></textarea>
+                </div>                        
+                <div class="form-group">
+                  <button type="submit" class="btn-submit">Send Now</button>
+                </div>
+              </form>   
+            </div>
+            <div class="col-sm-6">
+              <div class="contact-info wow fadeInUp" data-wow-duration="500ms" data-wow-delay="150ms">
+                <p></p>
+                <ul class="address">
+                  <li><i class="fa fa-map-marker"></i> <span>solusti</span> - 223 rue de Charenton - 75012 Paris - FRANCE</li>
+                  <li><i class="fa fa-map-signs"></i> <a href='https://maps.google.com/maps?ll=48.83815,2.390798&z=16&t=m&hl=fr&gl=US&mapclient=embed&daddr=223%20Rue%20de%20Charenton%2075012%20Paris%20France@48.8381501,2.3907978'>Get directions...</a> </li>
+                  <li><i class="fa fa-phone"></i> <span> Phone:</span> +33 (0)6 68 76 86 00</li>
+                  <li><i class="fa fa-envelope"></i> <span> Email:</span><a href="mailto:contact@solusti.com"> contact@solusti.com</a></li>
+                  <li><i class="fa fa-globe"></i> <span> Website:</span> <a href="#">www.solusti.com</a></li>
+                </ul>
+              </div>                            
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>        
+  </section><!--/#contact-->
+  <footer id="footer">
+    <div class="footer-top wow fadeInUp" data-wow-duration="500ms" data-wow-delay="150ms">
+      <div class="container text-center">
+        <div class="footer-logo">
+          <a href=""><img class="img-responsive" src="images/logo2_sm.png" alt=""></a>
+        </div>
+      </div>
+    </div>
+    <div class="footer-bottom">
+      
+    </div>
+  </footer>
+
+  <script type="text/javascript" src="js/jquery.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVl8u5JOgrFhwneD1TFmdzZizn-gmMadI"></script>
+  <script type="text/javascript" src="js/jquery.inview.min.js"></script>
+  <script type="text/javascript" src="js/wow.min.js"></script>
+  <script type="text/javascript" src="js/mousescroll.js"></script>
+  <script type="text/javascript" src="js/smoothscroll.js"></script>
+  <script type="text/javascript" src="js/jquery.countTo.js"></script>
+  <script type="text/javascript" src="js/lightbox.min.js"></script>
+  <script type="text/javascript" src="js/main.js"></script>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  
+    ga('create', 'UA-81709228-1', 'auto');
+    ga('send', 'pageview');
+  
+  </script>
+  
+</body>
+</html>
